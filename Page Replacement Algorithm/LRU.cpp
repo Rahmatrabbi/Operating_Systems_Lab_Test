15fafcd9817fe1180146f939ef62e01341504290
MD.RAHMAT-E-RABBI <48399993+Rahmatrabbi@users.noreply.github.com>
2023-04-03 12:41:11 +0600

    Update LRU.cpp

--- Page Replacement Algorithm/LRU.cpp
+++ Page Replacement Algorithm/LRU.cpp
@@ -1,53 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-bool contains(vector<pair<int,int>> frames_array, int n)
+bool contains(vector<pair<int, int>> frames_array, int n)
 {
-    for(auto frame: frames_array)
+    for (auto frame : frames_array)
     {
-        if(frame.first == n)
+        if (frame.first == n)
             return true;
     }
 
     return false;
 }
 
-int getIndex(vector<pair<int,int>> frames_array)
+int getIndex(vector<pair<int, int>> &frames_array)
 {
     int size = frames_array.size();
     int index = 0;
-    int smallest = frames_array[0].second;
+    int largest = frames_array[0].second;
 
-    for(int i = 1; i<size; i++)
+    for (int i = 1; i < size; i++)
     {
-        if(frames_array[i].second < smallest)
+        if (frames_array[i].second > largest)
         {
             index = i;
-            smallest = frames_array[i].second;
+            largest = frames_array[i].second;
         }
     }
 
     return index;
 }
 
+
 int main()
 {
     pair<int, int> frame;
     vector<int> reference_string = {7, 0, 1, 2, 0, 3, 0, 4, 2, 3, 0, 3, 2, 1, 2, 0, 1, 7, 0, 1};
-    vector<pair<int,int>> frames_array;
+    vector<pair<int, int>> frames_array;
     int frame_size = 3;
     int page_fault = 0;
     int i = 0;
     int replaced_index;
 
-    for(auto x: reference_string)
+    for (auto x : reference_string)
     {
-        if(!contains(frames_array, x))
+        if (!contains(frames_array, x))
         {
             page_fault++;
             cout << "Fault: " << x << " at:" << i << endl;
-            if(frames_array.size() < frame_size)
+            if (frames_array.size() < frame_size)
             {
-                frame = {x,0};
+                frame = {x, 0};
                 frames_array.push_back(frame);
             }
 
@@ -55,16 +56,28 @@ int main()
             {
                 replaced_index = getIndex(frames_array);
                 frame = {x, 0};
-                frames_array[i] = frame;
+                cout << "Victim: " << frames_array[replaced_index].first <<  endl;
+                frames_array[replaced_index] = frame;
+            }
+        }
+
+        else
+        {
+            for(auto &p: frames_array)
+            {
+                if(p.first == x)
+                {
+                    p.second = 0;
+                }
+            }
+        }
+
+            for (auto &p : frames_array)
+            {
+                p = make_pair(p.first, p.second + 1);
             }
 
-        }
-        i = i+1;
-
-        for(auto x: frames_array)
-        {
-            x.second = x.second+1;
-        }
+        ++i;
     }
 
     cout << "Total Page Faults are: " << page_fault;
